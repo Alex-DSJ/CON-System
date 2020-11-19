e2b8176893194a3de6890c4a264586a1a6de9649
Shijun Deng <dengshijun233@gmail.com>
2020-11-18 19:38:29 -0500

    create header and footer for each index, create the simple index page of the application, delete backend folder

---
+++ common/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>CON SYSTEM</title>
+
+    <!-- CSS libraries -->
+    <link href="https://cdn.bootcss.com/twitter-bootstrap/4.4.1/css/bootstrap.css" rel="stylesheet">
+
+</head>
+<style>
+    .m-t-10 {
+        margin-top: 10px;
+    }
+</style>
+<body class="hold-transition sidebar-mini"  style="background-color: #dddddd">
