387c15bf6dd516010fba6511b08944487d9c2ac3
kimchhengheng <hchheng_178@yahoo.com>
2020-11-29 11:39:34 -0500

    refactor some naming

--- func/db.php
+++ func/db.php
@@ -4,9 +4,10 @@
 try {
 
     $user = 'root';
-    $pass = 'Alex112524';
-    $host = '127.0.0.1';
+    $pass = 'hkc610787';
+    $host = '127.0.0.1:3307';
     $dbname = 'condo_system';
+
 
     global $db;
     $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $user, $pass);
